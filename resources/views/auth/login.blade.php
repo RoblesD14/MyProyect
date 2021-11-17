046bee64a9a7b05f920039a582dd6f8502292205
Monica Robles <mnrdrobles@gmail.com>
2021-11-17 16:38:41 +0000

    PServicios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                     <div class="col-lg-6">
                         <div class="p-5">
                             <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Sistema de Servicios!</h1>
+                                <h1 class="h4 text-gray-900 mb-4">SISTEMA DE SERVICIOS</h1>
                             </div>
                             <form method="POST" action="{{ route('login') }}" class="user">
                                 @csrf
